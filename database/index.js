b43a5ef8c6f2c211f88c616573a437b7e51a9de5
Daniel Torres <dani10sa2@hotmail.com>
2018-12-31 19:55:38 +0100

    Bugfix: connection to database fixed.

--- database/index.js
+++ database/index.js
@@ -3,16 +3,14 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
-const db = {};
+const connection_name = settings.mongodb.use;
 
-for (let connection_name in settings.mongodb.connections) {
-    const connection_data = settings.mongodb.connections[connection_name];
+const connection_data = settings.mongodb.connections[connection_name];
 
-    const { user, password, host, port, database} = connection_data;
-    const mongoDB = `mongodb://${user}:${password}@${host}:${port}/${database}`;
-    mongoose.connect(mongoDB, {useCreateIndex: true,useNewUrlParser: true, useFindAndModify: false});
-    db[connection_name] = mongoose.connection;
-    db[connection_name].on('error', console.error.bind(console, 'MongoDB connection error:'));
-}
+const { user, password, host, port, database} = connection_data;
+const mongoDB = user === null ? `mongodb://${host}:${port}/${database}` : `mongodb://${user}:${password}@${host}:${port}/${database}`;
+mongoose.connect(mongoDB, {useCreateIndex: true,useNewUrlParser: true, useFindAndModify: false});
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-module.exports = db[settings.mongodb.use];
+module.exports = db;
